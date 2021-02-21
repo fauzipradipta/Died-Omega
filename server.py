42420d2728a12f9ec816e3c2150f575339d2bb7b
esmeurena <33582708+esmeurena@users.noreply.github.com>
2021-02-21 03:57:36 -0800

    FINALLLLLLLLLLL
    Finished up user collection data, and website implementation

--- server.py
+++ server.py
@@ -9,6 +9,19 @@ import operator
 header = ['Age', 'Underlying', 'Fever', 'ShortnessOfBreath', 'LossOfTaste', 'SoreThroat', 'RiskFactor', 'Submit']
 data = []
 
+import pandas as pd 
+from pandas import DataFrame
+import seaborn as sns, numpy as np
+import matplotlib.pyplot as plt
+#setup data here from the site and utilizing pandas
+url = 'https://raw.githubusercontent.com/fauzipradipta/Died-Omega/main/covid_risk.csv'
+col_list = ['age', 'underlying', 'fever', 'ShortnessofBreath', 'LossofTaste', 'SoreThroat', 'RiskFactor ']
+
+data = pd.read_csv(url , usecols=col_list)
+data.head()
+df = pd.DataFrame(data)
+
+
 url1 = 'https://raw.githubusercontent.com/gheniabla/datasets/master/companylist-1.csv'
 response1 = urllib.request.urlopen(url1)
 lines = [l.decode('utf-8') for l in response1.readlines()]
@@ -42,13 +55,16 @@ def signup():
             writer.writerow(header)
             for key in result.keys():
                 csvDATA.write(result[key] + ' ')
+        with plt.rc_context({'axes.edgecolor':'black', 'xtick.color':'black', 'ytick.color':'black', 'figure.facecolor':'white'}):
+            plt.title('age vs. RiskFactor ')
+            plt.scatter(df.iloc[:,0], df.iloc[:,6])
+            plt.legend(['age'])
+            plt.show()
         return render_template('user.html', result=result)
     return render_template('signup2.html', form=form)
 
 if __name__ == '__main__':
     app.run()
-
-
 
 
 
